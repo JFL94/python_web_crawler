722203cda79cc0c123a7c6d440a5a281085f3d18
JFL94 <94jingfang@gmail.com>
2025-11-22 11:50:35 +0800

    modify

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,28 +1,32 @@
 from playwright.sync_api import sync_playwright
 from time import sleep
 
+# print(type(sync_playwright)) 
+
 def main():
     with sync_playwright() as p:
-        print(f"p={type(p)}")
-        # 啟動瀏覽器
-        browser = p.webkit.launch(headless=False)
+        # print("建立資源檔") # 連線時自動建立資源檔
         
-        print(f"browser:{type(browser)}")
+        # 啟動瀏覽器
+        browser = p.chromium.launch(headless=False)
+        print(type(browser))
+
         # 開啟新分頁
         page = browser.new_page()
 
-        print(f"page:{type(page)}")
-        
         # 訪問網站
         page.goto("https://www.google.com")
-        
-        # 取得標題
+
+        # 取得網站標題
         print(page.title())
 
-        sleep(20)
+        #
+        sleep(5)
         
         # 關閉瀏覽器
         browser.close()
+
+    print("釋放資源檔") # 離線時自動釋放資源檔
 
 if __name__ == "__main__":
     main()
